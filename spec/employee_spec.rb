36cb0a7eb27d3b1455097503e8c5aef3a3cb2fd4
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 19:44:12 -0600

    Edit #initialize test

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -7,7 +7,7 @@ describe Employee do
     it 'creates an instance object of Employee class' do
       bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
 
-      expect(employee).to be_an_instance_of(Employee)
+      expect(bobbi).to be_an_instance_of(Employee)
     end
   end
 

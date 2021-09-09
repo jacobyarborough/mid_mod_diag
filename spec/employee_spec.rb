ffbaf9d4c8622f32633243e0d21c6e16a71a5acb
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 19:48:54 -0600

    Create test for readable attrs

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -9,6 +9,14 @@ describe Employee do
 
       expect(bobbi).to be_an_instance_of(Employee)
     end
+
+    it 'has readable attributes' do
+      bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+
+      expect(bobbi.name).to eq("Bobbi Jaeger")
+      expect(bobbi.age).to eq(30)
+      expect(bobbi.salary).to eq(100000)
+    end
   end
 
 end

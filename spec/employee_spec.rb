8c8c8ece428b3bf22ad4fbaee0d8932694e89371
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 19:34:35 -0600

    Create #initialize test

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,1 +1,14 @@
- 
+require 'rspec'
+require './lib/employee'
+
+describe Employee do
+
+  describe '#initialize' do
+    it 'creates an instance object of Employee class' do
+      employee = Employee.new
+
+      expect(employee).to be_an_instance_of(Employee)
+    end
+  end
+
+end

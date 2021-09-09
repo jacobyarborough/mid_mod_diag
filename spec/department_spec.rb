e17432a5f112cfb898d264d2048f363370aaad2a
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 20:12:55 -0600

    Add #expense test

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -16,6 +16,7 @@ describe Department do
 
       expect(customer_service.name).to eq("Customer Service")
       expect(customer_service.employees).to eq([])
+      expect(customer_service.expenses).to eq(0)
     end
   end
 
@@ -28,6 +29,16 @@ describe Department do
       customer_service.hire(aaron)
 
       expect(customer_service.employees).to eq([bobbi, aaron])
+    end
+  end
+
+  describe '#expense' do
+    it 'adds an expense to expenses' do
+      customer_service = Department.new("Customer Service")
+      customer_service.expense(100)
+      customer_service.expense(25)
+
+      expect(customer_service.expenses).to eq(125)
     end
   end
 

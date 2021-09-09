a0d433fa0b66357404320c9691ba0147289c6b61
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 19:50:07 -0600

    Create attr_reader

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,9 +1,13 @@
 class Employee
 
+  attr_reader :name,
+              :age,
+              :salary
+
   def initialize(employee)
     @name = employee[:name]
-    @age = employee[:age]
-    @salary = employee[:salary]
+    @age = employee[:age].to_i
+    @salary = employee[:salary].to_i
   end
 
 end

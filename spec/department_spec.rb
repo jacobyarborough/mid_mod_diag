bbeb7d83b69fcd2fc4e23dc0e930a1342d98358b
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 20:02:22 -0600

    Create attr_reader test

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -10,6 +10,13 @@ describe Department do
 
       expect(customer_service).to be_an_instance_of(Department)
     end
+
+    it 'has readable attributes' do
+      customer_service = Department.new("Customer Service")
+
+      expect(customer_service.name).to eq("Customer Service")
+      expect(customer_service.employees).to eq([])
+    end
   end
 
 end

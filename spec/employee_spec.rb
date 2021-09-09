78b33c92d594b4ed7935f8950c38b6ea4ad9b394
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-08 19:36:25 -0600

    Refactor #initialize

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -3,9 +3,11 @@ require './lib/employee'
 
 describe Employee do
 
+  end
+
   describe '#initialize' do
     it 'creates an instance object of Employee class' do
-      employee = Employee.new
+      bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
 
       expect(employee).to be_an_instance_of(Employee)
     end
